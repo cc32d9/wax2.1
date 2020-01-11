b15fab063435c6fe8212e68b049c8bdb3275bc22
root <>
2020-01-11 12:44:54 +0000

    Generate: v2.0.0

--- docs/03_keosd/15_plugins/http_plugin.md
+++
@@ -1,3 +0,0 @@
----
-link: /01_nodeos/03_plugins/http_plugin/index.md
----
--- unittests/test-contracts/payloadless/payloadless.cpp
+++
@@ -1,7 +0,0 @@
-#include "payloadless.hpp"
-
-using namespace eosio;
-
-void payloadless::doit() {
-   print("Im a payloadless action");
-}
