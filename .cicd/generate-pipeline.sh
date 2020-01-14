1af4a5f16aaf07879723b757171c743d2583baa3
Nathan Pierce <nathan.pierce@block.one>
2020-01-14 17:33:49 -0500

    new branch name

--- .cicd/generate-pipeline.sh
+++ .cicd/generate-pipeline.sh
@@ -135,7 +135,7 @@ EOF
             - 'registry_2'
           pre-execute-sleep: 10
           pre-commands: 
-            - "rm -rf mac-anka-fleet; git clone git@github.com:EOSIO/mac-anka-fleet.git && cd mac-anka-fleet && . ./ensure-tag.bash -u 12 -r 25G -a '-n'"
+            - "rm -rf mac-anka-fleet; git clone git@github.com:EOSIO/mac-anka-fleet.git -b master-push-lock-file && cd mac-anka-fleet && . ./ensure-tag.bash -u 12 -r 25G -a '-n'"
       - thedyrt/skip-checkout#v0.1.1:
           cd: ~
     env:
