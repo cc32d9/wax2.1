3e257c974198a632803c7fddf82ca2a81c570148
Scott Arnette <scott.a.arnette@gmail.com>
2020-02-11 17:22:11 -0500

    Debug Anka LRTs.

--- .cicd/generate-pipeline.sh
+++ .cicd/generate-pipeline.sh
@@ -779,7 +779,7 @@ cat <<EOF
             - 'registry_2'
       - EOSIO/skip-checkout#v0.1.1:
           cd: ~
-    agents: "queue=mac-anka-node-fleet"
+    agents: "queue=mac-anka-large-node-fleet"
     retry:
       manual:
         permit_on_passed: true
