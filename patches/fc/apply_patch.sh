5fec036d4bf989ef5646833af477d42ca2fe361f
cc32d9 <cc32d9@gmail.com>
2021-05-30 10:28:34 +0000

    wax2.1: syntax fix and aligning unittests with eosio

--- patches/fc/apply_patch.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-TARGET=../../libraries/fc/CMakeLists.txt
-
-cp $TARGET ${TARGET}.bak_$(date +%s)
-patch $TARGET < ./CMakeLists.txt.patch
-
-echo Done.
--- samples/mainnet/cleos.sh
+++
@@ -1,1 +0,0 @@
-docker exec -ti nodeos bash -c "cleos $*" 
